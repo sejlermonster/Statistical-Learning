7b58fbe29602a036fd07ff68563c84b9011281c5
Simon Østergaard Kristensen <sok@sekoia.dk>
2017-04-10 14:40:36 +0200

    Multiple linear regression

---
+++ Lab363.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+
+boston = datasets.load_boston()
+
+lm = linear_model.LinearRegression()
+boston_X_train = boston.data
+boston_X_test = boston.data
+#for i in range(0,13):
+      #boston_X_train.append([item for sublist in boston.data[:, np.newaxis, i] for item in sublist]) 
+      #boston_X_test.append([item for sublist in boston.data[:, np.newaxis, i] for item in sublist]) 
+
+boston_Y_train = boston.target
+boston_Y_test = boston.target
+
+lm.fit(boston_X_train, boston_Y_train)
+
+# The coefficients
+print('Coefficients: \n', lm.coef_)
+# The mean squared error
+print("Mean squared error: %.2f"
+      % np.mean((lm.predict(boston_X_test) - boston_Y_test) ** 2))
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % lm.score(boston_X_test, boston_Y_test))
+
+#plt.scatter(boston_x, boston_Y_test,  color='black')
+#plt.plot(boston_X_test, lm.predict(boston_X_test), color='blue',
+ #       linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()

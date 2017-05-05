cf778ce170e176419fe62c5a7927835d1d0feebe
Simon Østergaard Kristensen <sok@sekoia.dk>
2017-05-05 09:41:10 +0200

    lab 562

--- Lab562.py
+++ Lab562.py
@@ -1,26 +1,27 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import pylab as pl
 from sklearn import metrics, linear_model
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import KFold, cross_val_score
 import pandas as pd
 
 data = pd.read_csv('Auto.csv', usecols=range(0,8), parse_dates=True)
 
 df = pd.DataFrame({'mpg':data['mpg'],'horsepower':  pd.to_numeric(data['horsepower'])})
-train, test = train_test_split(df, test_size = 0.5)
 
-X_train = train[['horsepower']]
-Y_train = train['mpg']
-
-X_test = test[['horsepower']]
-Y_test = test['mpg']
+X_train = df[['horsepower']]
+Y_train = df['mpg']
 
 #Linear regression
 lm = linear_model.LinearRegression()
 lm.fit(X_train, Y_train)
 
 print "\n"
+print "Intercept:"
+print lm.intercept_
+
+print "\n"
 print "Coefficients:"
 print lm.coef_
+
+#k_fold = KFold(n_splits=X_train.shape[0]) 
+test = cross_val_score(lm, X_train, Y_train, cv=192,  scoring = 'neg_mean_squared_error', n_jobs=-1)
+print np.mean(-test)

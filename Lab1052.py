63392e0a8b31ac53138ef5beca899953791f2bd8
Simon Østergaard Kristensen <sok@sekoia.dk>
2017-05-23 20:52:39 +0200

    Fix of 1052

--- Lab1052.py
+++ Lab1052.py
@@ -5,21 +5,18 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import scale
 
 # create 2 clusters of 25 data samples. THe two clusters can be identified by their different mean.
-mean1 = [-4, -4]
-mean2 = [3, 3]
-cov = [[1, 0], [0, 1]]
-x, y = np.random.multivariate_normal(mean1, cov, 25).T
-x1, y1 = np.random.multivariate_normal(mean2, cov, 25).T
-X = np.concatenate([x, x1])
-Y = np.concatenate([y, y1])
+# Generate data
+np.random.seed(3)
+X = np.random.standard_normal((50,2))
+X[:25,0] = X[:25,0]+3
+X[:25,1] = X[:25,1]-4
 
 #Using scaling before clustering
-scaled_x = scale(X)
-scaled_y = scale(Y)
-z_complete = (linkage(zip(scaled_x,scaled_y), method='complete', metric='euclidean'))
-#z_complete = linkage(zip(X,Y), method='complete', metric='euclidean')
-z_average = linkage(zip(X,Y), method='average', metric='euclidean')
-z_single = linkage(zip(X,Y), method='single', metric='euclidean')
+#scaled_x = scale(X)
+z_complete = (linkage(scaled_x, method='complete', metric='euclidean'))
+z_complete = linkage(X, method='complete', metric='euclidean')
+z_average = linkage(X, method='average', metric='euclidean')
+z_single = linkage(X, method='single', metric='euclidean')
 
 print cut_tree(z_complete, n_clusters=2).T
 print cut_tree(z_average, n_clusters=2).T
@@ -32,32 +29,32 @@ plt.title('Hierarchical Clustering Dendrogram')
 plt.xlabel('sample index')
 plt.ylabel('distance')
 dendrogram(
-    z_complete,
+    z_single,
     leaf_rotation=90.,  # rotates the x axis labels
     leaf_font_size=8.,  # font size for the x axis labels
 )
 
 
-# create 2 clusters of 25 data samples. THe two clusters can be identified by their different mean.
-mean = [0, 0]
-cov = [[1, 0], [0, 1]]
-x, y = np.random.multivariate_normal(mean, cov, 25).T
-x1, y1 = np.random.multivariate_normal(mean, cov, 25).T
-x2, y2 = np.random.multivariate_normal(mean, cov, 25).T
-X = np.concatenate([x, x1, x2])
-Y = np.concatenate([y, y1, y2])
+# # create 2 clusters of 25 data samples. THe two clusters can be identified by their different mean.
+# mean = [0, 0]
+# cov = [[1, 0], [0, 1]]
+# x, y = np.random.multivariate_normal(mean, cov, 25).T
+# x1, y1 = np.random.multivariate_normal(mean, cov, 25).T
+# x2, y2 = np.random.multivariate_normal(mean, cov, 25).T
+# X = np.concatenate([x, x1, x2])
+# Y = np.concatenate([y, y1, y2])
 
 
-dd = distance.pdist(1-np.corrcoef(zip(X,Y)))
-z_complete = linkage(dd, method='complete', metric='euclidean')
-# calculate full dendrogram
-plt.figure(figsize=(25, 10))
-plt.title('Hierarchical Clustering Dendrogram')
-plt.xlabel('sample index')
-plt.ylabel('distance')
-dendrogram(
-    z_complete,
-    leaf_rotation=90.,  # rotates the x axis labels
-    leaf_font_size=8.,  # font size for the x axis labels
-)
-plt.show()
+# dd = distance.pdist(1-np.corrcoef(zip(X,Y)))
+# z_complete = linkage(dd, method='complete', metric='euclidean')
+# # calculate full dendrogram
+# plt.figure(figsize=(25, 10))
+# plt.title('Hierarchical Clustering Dendrogram')
+# plt.xlabel('sample index')
+# plt.ylabel('distance')
+# dendrogram(
+#     z_complete,
+#     leaf_rotation=90.,  # rotates the x axis labels
+#     leaf_font_size=8.,  # font size for the x axis labels
+# )
+# plt.show()
